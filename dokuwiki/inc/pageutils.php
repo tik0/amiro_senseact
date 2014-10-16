f4029fdcbe6c78a4d2889917d95db7be8d2ee29e
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2014-10-16 17:05:37 +0200

    Added camelcase folder ability

--- dokuwiki/inc/pageutils.php
+++ dokuwiki/inc/pageutils.php
@@ -118,7 +118,7 @@ function cleanID($raw_id,$ascii=false){
         $sepcharpat = '#\\'.$sepchar.'+#';
 
     $id = trim((string)$raw_id);
-    $id = utf8_strtolower($id);
+//     $id = utf8_strtolower($id);
 
     //alternative namespace seperator
     if($conf['useslash']){
