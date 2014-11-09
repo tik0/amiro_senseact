8ca65c7ca36f07c2cc054504c2aa3964d63c6022
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2014-11-09 15:52:29 +0100

    Just reformating to the google style

--- project/tools/localizationTwb/object.h
+++ project/tools/localizationTwb/object.h
@@ -1,22 +1,21 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-
 #define   OBJECT_MAX       30
 
 typedef struct {
-    char       name[256];
-    int        id;
-    int        visible;
-	int		   collide;
-    double     marker_coord[4][2];
-    double     trans[3][4];
-    double     marker_width;
-    double     marker_center[2];
-	double	   marker_grad;
-	double	   marker_pos[2];
+  char name[256];
+  int id;
+  int visible;
+  int collide;
+  double marker_coord[4][2];
+  double trans[3][4];
+  double marker_width;
+  double marker_center[2];
+  double marker_grad;
+  double marker_pos[2];
 } ObjectData_T;
 
-ObjectData_T  *read_ObjData( char *name, int *objectnum );
+ObjectData_T *read_ObjData(char *name, int *objectnum);
 
 #endif
