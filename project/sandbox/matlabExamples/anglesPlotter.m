d62ed03fdeb1779fa6e21a736e35792cb8a03a4e
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2014-12-17 02:24:24 +0100

    No need

--- project/sandbox/matlabExamples/anglesPlotter.m
+++
@@ -1,8 +0,0 @@
-function [] = anglesPlotter( scope )
-%ANGLESPLOTTER Simple live plot of joint angles
-%   Scope must map to a rst.kinematics.JointAngles type
-angles = rsb.matlab.JointAnglesQueue;
-createListener(scope, angles);
-plotAngles(angles);
-end
-
