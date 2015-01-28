8ccad62d49908ada80cc032b4d5d0dc5a9683db4
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2015-01-28 12:58:52 +0100

    Cleanup and fix of high cpu load

--- project/tools/sendControls/main.cxx
+++ project/tools/sendControls/main.cxx
@@ -6,9 +6,6 @@
 #define ERROR_MSG_
 #include <MSG.h>
 
-// Reading inScope and outScope
-#include <oFlags.hpp>
-
 #include <iostream>
 #include <boost/thread.hpp>
 #include <boost/program_options.hpp>
@@ -24,9 +21,6 @@
 // Include own converter
 #include <converter/vecIntConverter/main.hpp>
 
-
-
-
 using namespace boost;
 using namespace std;
 //using namespace cv;
@@ -40,28 +34,43 @@ using namespace rsb::converter;
 
 int main(int argc, const char **argv){
   
-      INFO_MSG( "Before reading values " )
-      
-      INFO_MSG( "Outscope: " << oFlags::outScope)
-      
-      oFlags::handleCommandline(argc, argv);
-      INFO_MSG( "After reading values " )
-      
-      INFO_MSG( "Outscope: " << oFlags::outScope)
-      
-      
-    // Get the RSB factory
-    rsb::Factory& factory = rsb::Factory::getInstance();
-    
-    // Register new converter for std::vector<int>
-    shared_ptr<vecIntConverter> converterVecInt(new vecIntConverter());
-    converterRepository<std::string>()->registerConverter(converterVecInt);
+  // Handle program options
+  namespace po = boost::program_options;
+  
+  std::string outScope = "/motor";
 
-    // Prepare RSB informer
-    rsb::Informer< std::vector<int> >::Ptr informer_vec = factory.createInformer< std::vector<int> > (oFlags::outScope);
+  po::options_description options("Allowed options");
+  options.add_options()("help,h", "Display a help message.")
+    ("outscope,o", po::value < std::string > (&outScope), "Scope for sending the motor steering commands");
 
-    // Calculate the new steering (two elements with 0 initialized)
-    boost::shared_ptr< std::vector<int> > vecSteering(new std::vector<int> (2,0));
+  // allow to give the value as a positional argument
+  po::positional_options_description p;
+  p.add("value", 1);
+
+  po::variables_map vm;
+  po::store( po::command_line_parser(argc, argv).options(options).positional(p).run(), vm);
+
+  // first, process the help option
+  if (vm.count("help")) {
+      std::cout << options << "\n";
+      exit(1);
+  }
+
+  // afterwards, let program options handle argument errors
+  po::notify(vm);
+
+  // Get the RSB factory
+  rsb::Factory& factory = rsb::Factory::getInstance();
+
+  // Register new converter for std::vector<int>
+  boost::shared_ptr<vecIntConverter> converterVecInt(new vecIntConverter());
+  converterRepository<std::string>()->registerConverter(converterVecInt);
+
+  // Prepare RSB informer
+  rsb::Informer< std::vector<int> >::Ptr informer_vec = factory.createInformer< std::vector<int> > (outScope);
+
+  // Calculate the new steering (two elements with 0 initialized)
+  boost::shared_ptr< std::vector<int> > vecSteering(new std::vector<int> (2,0));
 
    int KB_code=0;
 
@@ -70,46 +79,47 @@ int main(int argc, const char **argv){
      if (kbhit())
       {
             KB_code = getchar();
-	    INFO_MSG( "KB_code = " << KB_code )
+            INFO_MSG( "KB_code = " << KB_code )
 
             switch (KB_code)
             {
                 case KB_A:
-			   // Degree per second
-                           vecSteering->at(1) = vecSteering->at(1) + 10;
-			   informer_vec->publish(vecSteering);
+                  // Degree per second
+                  vecSteering->at(1) = vecSteering->at(1) + 10;
+                  informer_vec->publish(vecSteering);
                 break;
 
                 case KB_D:
-		           // Degree per second
-                           vecSteering->at(1) = vecSteering->at(1) - 10;
-			   informer_vec->publish(vecSteering);
+                  // Degree per second
+                  vecSteering->at(1) = vecSteering->at(1) - 10;
+                  informer_vec->publish(vecSteering);
                 break;
 
                 case KB_W:
-		           // Milimeter per second
-                           vecSteering->at(0) = vecSteering->at(0) + 10;
-			   informer_vec->publish(vecSteering);
+                  // Milimeter per second
+                  vecSteering->at(0) = vecSteering->at(0) + 10;
+                  informer_vec->publish(vecSteering);
                 break;
 
                 case KB_S:
-			   // Milimeter per second
-                           vecSteering->at(0) = vecSteering->at(0) - 10;
-			   informer_vec->publish(vecSteering);
+                  // Milimeter per second
+                  vecSteering->at(0) = vecSteering->at(0) - 10;
+                  informer_vec->publish(vecSteering);
                 break;
-		case KB_SPACE:
-			   vecSteering->at(0) = 0;
-			   vecSteering->at(1) = 0;
-			   informer_vec->publish(vecSteering);
+                case KB_SPACE:
+                  vecSteering->at(0) = 0;
+                  vecSteering->at(1) = 0;
+                  informer_vec->publish(vecSteering);
                 break;
-		case KB_ENTER:
-			   informer_vec->publish(vecSteering);
+                case KB_ENTER:
+                  informer_vec->publish(vecSteering);
                 break;
 
             }        
-            INFO_MSG( " v = " << vecSteering->at(0) << ", w = " << vecSteering->at(1) )
-
+            INFO_MSG( "Vx  = " << vecSteering->at(0) << " m/s , Wz = " << vecSteering->at(1) << " rad/s" )
       }
+      // Sleep for a millisecond
+      usleep(1000);
   }
 
   return 0;
