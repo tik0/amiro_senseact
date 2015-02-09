9f862fb5849c3232451ae30a4984c07467797264
Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>
2015-02-09 23:28:02 +0100

    Add credentials for www.multirobotix.[de|com]

--- dokuwiki/lib/plugins/source/conf/default.php
+++ dokuwiki/lib/plugins/source/conf/default.php
@@ -6,7 +6,7 @@
  */
 
 // location is prepended to all file names, restricting the filespace exposed to the plugin
-$conf['location'] = 'http://localhost/project/';
+$conf['location'] = 'http://murox:murox@www.multirobotix.com/project/';
 
 // if allow array contains any elements, ONLY files with the extensions listed will be allowed
 $conf['allow'] = '';
