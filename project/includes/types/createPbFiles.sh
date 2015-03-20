fdbd4b1f8fa625e048b6880fe85a6190e0397910
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2015-03-20 15:50:33 +0100

    include: types: Fix proto generater script

--- project/includes/types/createPbFiles.sh
+++ project/includes/types/createPbFiles.sh
@@ -3,6 +3,6 @@ echo "Running protoc for every *.proto file to create the *pb.cc and *pb.h files
 # To be more explicit, we run every file to get propter output
 for list in `ls *.proto`
 do
-  echo "protoc ${list}.proto --cpp_out=."
-  protoc ${list} --cpp_out=.
+  echo "protoc ${list} --cpp_out=."
+  protoc ${list} --cpp_out=. --proto_path=. --proto_path=/usr/local/share/rst0.11/proto/stable/ --proto_path=/usr/local/share/rst0.11/proto/sandbox/
 done
