f651eaf02b932a1f7e767b93794c0b410535992a
Alexander Bauer <albauer@techfak.uni-bielefeld.de>
2015-03-24 13:53:19 +0100

    zbar QRCode reader integrated and CMakeLists.txt adapted.

--- project/demo/startChoreo/main.cxx
+++ project/demo/startChoreo/main.cxx
@@ -8,7 +8,13 @@
 
 #include <iostream>
 #include <string>
+#include <zbar.h>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
 using namespace std;
+using namespace zbar;
 
 // boost
 #include <boost/shared_ptr.hpp>
@@ -33,8 +39,8 @@ int main(int argc, char **argv) {
 	// Handle program options
 	po::options_description options("Allowed options");
 	options.add_options()("help,h", "Display a help message.")
-		("name,n",po::value<std::string>(&choreoName),"Name of the choreography.")
-			("choreoOut", po::value<std::string>(&choreoOutscope),"Choreography outscope.");
+				("name,n",po::value<std::string>(&choreoName),"Name of the choreography.")
+				("choreoOut", po::value<std::string>(&choreoOutscope),"Choreography outscope.");
 
 	// allow to give the value as a positional argument
 	po::positional_options_description p;
@@ -56,9 +62,74 @@ int main(int argc, char **argv) {
 	// prepare RSB informer for choreos
 	Informer<string>::Ptr choreoInformer = factory.createInformer<string>(choreoOutscope);
 
-	// publish the data
-	Informer<string>::DataPtr message(new string(choreoName));
-	choreoInformer->publish(message);
+	// Open camera no. 0 and check for success
+	VideoCapture cap(0);
+	if (!cap.isOpened()) // if not success, exit program  
+	{  
+		cout << "Cannot open the video cam. Exiting..." << endl;
+		return -1;  
+	}
+
+	// Create window for camera stream
+	namedWindow("Videostream",CV_WINDOW_AUTOSIZE);
+
+	Mat img, augmented;
+
+	// Create zbar image scanner
+	ImageScanner scanner;
+	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+
+	bool codeFound = false;
+
+	// Endlessly read image from camera
+	while(!codeFound)
+	{
+		if (!cap.read(img)) //if not success, break loop
+		{  
+			cout << "Cannot read frame from camera. Exiting..." << endl;
+			break;  
+		}
+
+		// Wrap image data
+		augmented = img.clone(); // clone before converting
+		cvtColor(img,img,CV_BGR2GRAY);
+		uchar *raw = (uchar *)img.data;
+		int width = img.cols;
+		int height = img.rows;
+		Image image(width, height, "Y800", raw, width * height);
+
+		// Scan the image for codes
+		scanner.scan(image);
+
+		// Extract results
+		for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+		{   
+			choreoName = symbol->get_data();
+			cout << "Decoded " << symbol->get_type_name() << " symbol \"" << symbol->get_data() << '"' <<" "<< endl;
+			codeFound = true;
+
+			// Draw rectangle around QR code
+			vector<Point> vp;
+			int n = symbol->get_location_size();
+			for(int i = 0; i < n; i++) vp.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i)));
+			RotatedRect r = minAreaRect(vp);
+			Point2f pts[4];   
+			r.points(pts);   
+			for(int i=0;i<4;i++) line(augmented,pts[i],pts[(i+1)%4],Scalar(255,0,0),3);
+		}    
+
+		imshow("Videostream",augmented);
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop  
+		{
+			cout << "esc key is pressed by user. Exiting..." << endl;
+			break;   
+		}  
+
+		// Publish the data
+		Informer<string>::DataPtr message(new string(choreoName));
+		choreoInformer->publish(message);
+	}
 
 	return EXIT_SUCCESS;
 }
