09b49a72dc2ad41e924361eafba158e80d17fce7
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-04-14 12:17:20 +0200

    Included Homing into choreographer

--- project/demo/choreoStateMachine/main.cxx
+++ project/demo/choreoStateMachine/main.cxx
@@ -43,6 +43,21 @@ using namespace rsb;
 #endif
 
 #include <choreo.h>
+#include <Types.h>
+
+
+
+#ifndef SIMULATION
+	// initialize CAN
+    ControllerAreaNetwork myCAN;
+#else
+    // initialize CAN MockUp
+    CANMockUp myCAN;
+#endif
+
+// variables for homing procedure
+bool doHoming = false;
+types::position homingPosition;
 
 
 // load a choreography from a file
@@ -146,6 +161,76 @@ Choreo loadChoreo(std::string choreoName) {
 	return choreo;
 }
 
+
+void moveToTargetPosition(types::position position, int16_t timeMS) {
+	// initialize velocities
+	int32_t v = 0;
+	int32_t w = 0;
+
+	// send target position (if not set, then again)
+	do {
+		myCAN.setTargetPosition(position, timeMS);
+		usleep(500000);
+		myCAN.getActualSpeed(v, w);
+		usleep(250000);
+	} while (v==0 && w==0);
+
+	// wait for finished movement
+        // TODO implement better termination condition when possible by CAN interface
+	do {
+		usleep(250000);
+		myCAN.getActualSpeed(v, w);
+		if (v==0 && w==0) {
+			usleep(500000);
+			myCAN.getActualSpeed(v, w);
+		}
+	} while (v != 0 || w != 0);
+}
+
+
+void homing() {
+	// get odometry data
+	homingPosition = myCAN.getOdometry();
+
+	// calculate angle from robot position (without respect to orientation) to origin (in rad)
+	double angleOdo = atan2((double)homingPosition.y, (double)homingPosition.x) + M_PI;
+	if (angleOdo > 2*M_PI) {
+		angleOdo -= 2*M_PI;
+	}
+
+	// calculate difference angle (in urad) to turn towards origin
+	int diffAngle = (int)(angleOdo*1e6)-homingPosition.f_z;
+
+	// calculate angle (in urad) to turn to origin orientation after reaching origin
+	int backAngle = -homingPosition.f_z-diffAngle;
+	while (backAngle < 0) {
+		backAngle += 2*M_PI*1e6;
+	}
+	while (backAngle > M_PI*1e6) {
+		backAngle -= 2*M_PI*1e6;
+	}
+
+	// calculate distance between origin and robot position (in um)
+	int diffDist = (int) sqrt((double)homingPosition.y*(double)homingPosition.y + (double)homingPosition.x*(double)homingPosition.x);
+
+	// reset position struct
+	homingPosition.y = 0;
+
+	// turn towards origin
+	homingPosition.x = 0;
+	homingPosition.f_z = diffAngle;
+	moveToTargetPosition(homingPosition, 1);
+	// drive to origin
+	homingPosition.x = diffDist;
+	homingPosition.f_z = 0;
+	moveToTargetPosition(homingPosition, 1);
+        // turn towards origin orientation
+	homingPosition.x = 0;
+	homingPosition.f_z = backAngle;
+	moveToTargetPosition(homingPosition, 1);
+}
+
+
 int main(int argc, char **argv) {
 
 	// scopenames for rsb
@@ -163,7 +248,8 @@ int main(int argc, char **argv) {
 		("verbose,v","Print values of the choreography.")
 			("choreoIn", po::value<std::string>(&choreoInscope),"Choreography inscope.")
 			("delay",po::value<int>(&delay),"Dealy between creating the rsb event and starting the choreography in ms.")
-			("pos",po::value<int>(&pos),"Position in the formation (from the front: 0=left, 1=middle left, 2=middle, 3=middle right, 4=right)");
+			("pos",po::value<int>(&pos),"Position in the formation (from the front: 0=left, 1=middle left, 2=middle, 3=middle right, 4=right)")
+                        ("homing,s","The homing will be activated. After finishing the choreography the robot will return to its start position.");
 
 	// allow to give the value as a positional argument
 	po::positional_options_description p;
@@ -179,6 +265,9 @@ int main(int argc, char **argv) {
 	// afterwards, let program options handle argument errors
 	po::notify(vm);
 
+        // check if homing should be done
+        doHoming = vm.count("homing");
+
 	// Get the RSB factory
 	rsb::Factory& factory = rsb::Factory::getInstance();
 
@@ -186,14 +275,6 @@ int main(int argc, char **argv) {
 	rsb::ListenerPtr choreoListener = factory.createListener(choreoInscope);
 	boost::shared_ptr<rsc::threading::SynchronizedQueue<EventPtr>>choreoQueue(new rsc::threading::SynchronizedQueue<EventPtr>(1));
 	choreoListener->addHandler(rsb::HandlerPtr(new rsb::util::EventQueuePushHandler(choreoQueue)));
-
-#ifndef SIMULATION
-	// initialize CAN
-    ControllerAreaNetwork myCAN;
-#else
-    // initialize CAN MockUp
-    CANMockUp myCAN;
-#endif
 
     // initialize the robots attributes
     int v = 0;
@@ -215,12 +296,20 @@ int main(int argc, char **argv) {
 		std::stringstream fileNameStream;
 		fileNameStream << songName << pos << ".xml";
 		cout << "Filename: " << fileNameStream.str() << endl;
-        Choreo choreo = loadChoreo(fileNameStream.str());
+                Choreo choreo = loadChoreo(fileNameStream.str());
 
 		// wait for choreo to begin
 		boost::this_thread::sleep_until(nextStepTime);
 
-		// preform the choreo
+		// reset odometry
+		if (doHoming) {
+			homingPosition.x = 0;
+			homingPosition.y = 0;
+			homingPosition.f_z = 0;
+			myCAN.setOdometry(homingPosition);
+		}
+
+		// perform the choreo
 		for (ChoreoStep cs : choreo) {
 
 			// update values only if they have changed
@@ -260,8 +349,10 @@ int main(int argc, char **argv) {
 			boost::this_thread::sleep_until(nextStepTime);
 		}
 
-		// choreo finished
-		// TODO drive back to start?
+		// do final homing
+		if (doHoming) {
+			homing();
+		}
 	}
 
 
