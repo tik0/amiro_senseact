b1728a01225c747363b0caad87707efcb8c14e98
fpatzelt <fpatzelt@techfak.uni-bielefeld.de>
2015-04-17 13:10:54 +0200

    Added model and runscript for a tilted laserscanner

---
+++ project/demo/CoreSLAMSim/run_tilted.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start the spread communication deamon
+spread -c spreadSim.conf &
+sleep 5
+# Start the simulator
+gazebo &
+# Start the SLAM program
+./CoreSLAM/CoreSLAM --odominscope /AMiRo_Hokuyo_tilted/gps/ --lidarinscope /AMiRo_Hokuyo_tilted/lidar/ --senImage 1 --delta 0.01 &
+echo "Enabled steering by wasd, enter (resend), space (halt)"
+./sendTwistControls/sendTwistControls -o /AMiRo_Hokuyo_tilted/diffKin
