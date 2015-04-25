8860590a0291c466f629a846db8027b6a2eb4a3f
fpatzelt <fpatzelt@techfak.uni-bielefeld.de>
2015-04-25 21:27:28 +0200

    Implemented functional waypoint logic with filtering of the initial scan.

--- project/sandbox/waypoint/main.cxx
+++ project/sandbox/waypoint/main.cxx
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-//#define INFO_MSG_
+#define INFO_MSG_
 #define DEBUG_MSG_
 // #define SUCCESS_MSG_
 // #define WARNING_MSG_
@@ -45,22 +45,23 @@ using namespace rsb::patterns;
 //        data.d[i] = (int) (scan.scan_values(i)*METERS_TO_MM);
 //    }
 //
-int main(int argc, const char **argv) {
 
+int main(int argc, const char **argv) {
+	DEBUG_MSG("Start waypoint")
 	namespace po = boost::program_options;
 	std::string lidarInScope = "/AMiRo_Hokuyo/lidar";
-	std::string stateOutScope = "checkpoint/state";
-	std::string commandInscope = "checkpoint/command";
-	double range = 1.0;
-	double diffThreshold = 0.3;
+	std::string stateOutScope = "/waypoint/state";
+	std::string commandInscope = "/waypoint/command";
+	float range = 1.0;
+	float diffThreshold = 0.3;
 
 	po::options_description options("Allowed options");
 	options.add_options()("help,h", "Display a help message.")
 			("lidarinscope",po::value<std::string>(&lidarInScope),"Scope for receiving lidar data")
 			("stateoutscope",po::value<std::string>(&stateOutScope), "Scope for sending states")
 			("commandinscope", po::value<std::string>(&commandInscope),"Scope for receiving commands")
-			("range,r",po::value<double>(&range), "Range of detection in m")
-			("diffThreshold",po::value<double>(&diffThreshold), "Difference threshold in m");
+			("range,r",po::value<float>(&range), "Range of detection in m")
+			("diffThreshold",po::value<float>(&diffThreshold), "Difference threshold in m");
 
 	// allow to give the value as a positional argument
 	po::positional_options_description p;
@@ -94,23 +95,36 @@ int main(int argc, const char **argv) {
 
 	rsb::ListenerPtr commandListner = factory.createListener(commandInscope);
 	boost::shared_ptr<rsc::threading::SynchronizedQueue<boost::shared_ptr<std::string>>>commandQueue(new rsc::threading::SynchronizedQueue<boost::shared_ptr<std::string>>(1));
-	lidarListener->addHandler(rsb::HandlerPtr(new rsb::util::QueuePushHandler<std::string>(commandQueue)));
+	commandListner->addHandler(rsb::HandlerPtr(new rsb::util::QueuePushHandler<std::string>(commandQueue)));
 
 	rsb::Informer<std::string>::Ptr stateInformer = factory.createInformer<std::string>(stateOutScope);
 
-	rst::vision::LocatedLaserScan scan, initscan;
+	rst::vision::LocatedLaserScan scan, initscan, initscan_raw;
 
-	bool enabled = true;
+	bool enabled = false;
 	bool triggered = false;
 
 	while (true) {
 		// check if the checkpoint is enabled/disabled by the stateMachine
-		if (enabled) {
+		if (!enabled) {
 			std::string command(*commandQueue->pop());
 			if (command.compare("init") == 0) {
+				DEBUG_MSG("init")
 				enabled = true;
 				// initialize scan
-				initscan = *lidarQueue->pop();
+				initscan_raw = *lidarQueue->pop();
+				for (int i = 0; i < initscan_raw.scan_values_size(); ++i) {
+					if (initscan_raw.scan_values(i) < initscan_raw.scan_values_min()) {
+						initscan_raw.set_scan_values(i,10000);
+					}
+				}
+				// filter initial scan
+				for (int i = 1; i < initscan.scan_values_size()-1; ++i) {
+					initscan.set_scan_values(i,min(initscan_raw.scan_values(i-1),min(initscan_raw.scan_values(i),initscan_raw.scan_values(i+1))));
+					if (initscan.scan_values(i) == 10000) {
+						initscan.set_scan_values(i,0);
+					}
+				}
 			}
 		} else {
 			if (!commandQueue->empty()) {
@@ -126,23 +140,24 @@ int main(int argc, const char **argv) {
 
 		// check if the waypoint is triggered
 		bool triggered_new = false;
-		for (int i = 0; i < scan.scan_values_size(); ++i) {
-			if (scan.scan_values(i) < range
-					&& abs(scan.scan_values(i) - initscan.scan_values(i))
-							> diffThreshold) {
-				triggered = true;
+		for (int i = 1; i < scan.scan_values_size()-1; ++i) {
+			if (scan.scan_values(i) > scan.scan_values_min() && initscan.scan_values(i) > initscan.scan_values_min() && scan.scan_values(i) < range
+					&& initscan.scan_values(i) - scan.scan_values(i)> diffThreshold) {
+				//DEBUG_MSG(abs(scan.scan_values(i) - initscan.scan_values(i)));
+				triggered_new = true;
 				break;
 			}
 		}
-
 		// send update state
 		if (triggered != triggered_new) {
 			if (triggered_new) {
 				stateInformer->publish(
 						Informer<string>::DataPtr(new string("entered")));
+				DEBUG_MSG("entered");
 			} else {
 				stateInformer->publish(
 						Informer<string>::DataPtr(new string("left")));
+				DEBUG_MSG("left");
 			}
 			triggered = !triggered;
 		}
