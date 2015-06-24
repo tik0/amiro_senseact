3b4c8f8f38d97ffae427c61d2acd661f6d7055eb
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2015-06-24 14:24:29 +0200

    demo: robocup: Depricated

--- project/demo/robocup/standby.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for ID in {0..9}; do
-  echo "Send 'standby' to ID ${ID}"
-  rsb-sendcpp0.9 /tabletop/BB${ID}/standby void
-done
-exit 0
--- project/demo/robocup/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for ID in {0..9}; do
-  echo "Send 'start' to ID ${ID}"
-  rsb-sendcpp0.9 /tabletop/BB${ID}/start void
-done
-exit 0
-
-# tiko@lappy:~/repositories/murox/project/sandbox/TemplateMatchingWithBeBotCam$ rsb-sendcpp0.9 /tabletop/BB2/standby void
