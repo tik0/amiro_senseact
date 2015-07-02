017d072b38a5040eb17328786c4af31e7175b7b7
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-07-02 22:47:57 +0200

    Added new demo for openChallenge with exploration.

---
+++ project/demo/openChallenge2015_onlyExplo/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+ID=${1}
+
+./stop.sh
+
+cpufreq-set -g performance
+
+spread -c amirospread &
+sleep 5
+
+./senseHokuyo -d /dev/ttyACM0 -o /lidar --publishNthScan 3 > /dev/null &
+./senseOdometry -o /odom &
+./rirReader -l >> /dev/null &
+./exploDriveEdge -l >> /dev/null &
+./CoreSLAM --lidarinscope /lidar --odominscope /odom --serverScope /CoreSlamServer --mapServerReq map --remoteHost localhost --remotePort 4823 --senImage 0
+./answerer --skipExploration >> /dev/null &
+./openChallengeGEPTRO_2nd --robotID ${ID} --testWithAnswerer &
+sleep 1
+./answerer_tobi --robotID ${ID} &
+
+wait
+cpufreq-set -g ondemand
+
---
+++ project/demo/openChallenge2015_onlyExplo/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+killall -9 openChallengeGEPTRO_2nd
+killall -9 answerer
+killall -9 answerer_tobi
+killall -9 CoreSLAM
+killall -9 exploDriveEdge
+killall -9 rirReader
+killall -9 senseOdometry
+killall -9 senseHokuyo
+killall -9 spread
