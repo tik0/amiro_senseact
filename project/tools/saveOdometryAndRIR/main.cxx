0b32bd5e7ca24883f0c7126e8b2521e99d6c976f
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-07-02 12:57:38 +0200

    Added saveOdometryAndRIR to tools and build in command line options.

--- project/tools/saveOdometryAndRIR/main.cxx
+++ project/tools/saveOdometryAndRIR/main.cxx
@@ -16,6 +16,11 @@
 #include <algorithm>
 #include <math.h>
 
+#include <iostream>
+#include <boost/thread.hpp>
+#include <boost/program_options.hpp>
+#include <boost/shared_ptr.hpp>
+
 #include <rsb/filter/OriginFilter.h>
 #include <rsb/Informer.h>
 #include <rsb/Factory.h>
@@ -47,6 +52,45 @@ std::string proxSensorInscope = "/rir_prox";
 std::string odometryInscope = "/odometrydata";
 
 int main(int argc, char **argv) {
+
+	// Handle program options
+	namespace po = boost::program_options;
+
+	po::options_description options("Allowed options");
+	options.add_options()("help,h", "Display a help message.")
+		("rirScope,r", po::value<std::string> (&proxSensorInscope), "Scope for receiving proximity values for obstacle.")
+		("odoScope,o", po::value<std::string> (&odometryInscope), "Scope for receiving odometry data.")
+		("init,i", "Just initializes data file.");
+
+	// allow to give the value as a positional argument
+	po::positional_options_description p;
+	p.add("value", 1);
+
+	po::variables_map vm;
+	po::store( po::command_line_parser(argc, argv).options(options).positional(p).run(), vm);
+
+	// first, process the help option
+	if (vm.count("help")) {
+		std::cout << options << "\n";
+		exit(1);
+	}
+
+	// afterwards, let program options handle argument errors
+	po::notify(vm);
+
+	if (vm.count("init")) {
+		FILE *file;
+		file = fopen("RingProximityAndOdometry.txt", "w");
+		for (int step=0; step<8; step++) {
+			fprintf(file, "PRV %i\t", step);
+		}
+		fprintf(file, "X [ym]\tY [ym]\tTheta [yrad]\n");
+		fclose(file);
+
+        	INFO_MSG("File has been initialized");
+
+		return EXIT_SUCCESS;
+	}
 
 	// Get the RSB factory
 	rsb::Factory& factory = rsb::Factory::getInstance();
