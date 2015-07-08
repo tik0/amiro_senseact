0fc41b83e9fa741765d3053021ecd3484daa0eb3
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2015-07-08 08:46:44 +0200

    process: CoreSLAMBasic: Add function for obstacle map retrievement and arguments for erision of obstacle for the regarding tasks (pathObstacleErosion, detectionObstacleErosion)

--- project/process/CoreSLAMBasic/main.cxx
+++ project/process/CoreSLAMBasic/main.cxx
@@ -127,6 +127,27 @@ std::mutex map_to_odom_mutex_;
 std::mutex mtxOdom;       // mutex for odometry messages
 static rst::geometry::Translation odomTrans;
 static rst::geometry::Rotation odomRot;
+static double pathObstacleErosion = 0.1; // m
+static double detectionObstacleErosion = 0.1; // m
+
+// Erode the map by with an eliptic pattern of pixel radius erosion_size = <size in meter> / delta_;
+void mapErosion(int erosion_size, cv::Mat &map) {
+  cv::Mat element = cv::getStructuringElement( cv::MORPH_ELLIPSE,
+                                             cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                                             cv::Point( erosion_size, erosion_size ) );
+  cv::erode( map, map, element );
+}
+
+cv::Mat getObstacleMap() {
+  // Convert the map to a cv::Mat image
+  cv::Mat map(cv::Size(TS_MAP_SIZE,TS_MAP_SIZE), CV_8UC1); // destination image for sending
+  cv::Mat tmp = cv::Mat(TS_MAP_SIZE, TS_MAP_SIZE, CV_16UC1, static_cast<void*>(&ts_map_.map[0]));
+  tmp.convertTo(map, CV_8UC1, 0.00390625);  // Convert to 8bit depth image
+  for (ssize_t idx = 0; idx < map.rows * map.cols; ++idx)
+    if (map.at<uint8_t>(idx) > 127) // Delete all unsure guesses
+      map.at<uint8_t>(idx) = 0xFF;
+  return map;
+}
 
 // Show an image in a window with the given name.
 // The image will be flipped along its x-axis.
@@ -152,7 +173,9 @@ class pathCallback: public rsb::patterns::LocalServer::Callback<twbTracking::pro
 			boost::shared_ptr<twbTracking::proto::Pose2D> pose) {
 
 		// generate the obstacle map
-		Mat obstacleMap;
+		cv::Mat obstacleMap(getObstacleMap());
+		const int erosion_size = pathObstacleErosion / delta_;
+		mapErosion(erosion_size, obstacleMap);
 // 1. TODO	mapGenerator.generateObstacleMap(combinedMap, obstacleMap);
 
 		// convert pose to cv::point2f
@@ -165,6 +188,7 @@ class pathCallback: public rsb::patterns::LocalServer::Callback<twbTracking::pro
 		mtxOdom.unlock();
 
 		// calculate a path
+		// Erode the map by with an eliptic pattern of pixel radius erosion_size = <size in meter> / delta_;
 		std::vector<cv::Point2f> path = pathPlanner->getPathToTarget(obstacleMap, robotPose, target);
 
 		// convert that path to a pose2DList
@@ -186,81 +210,72 @@ class pathCallback: public rsb::patterns::LocalServer::Callback<twbTracking::pro
 class objectsCallback: public rsb::patterns::LocalServer::Callback<bool, twbTracking::proto::Pose2DList> {
   boost::shared_ptr<twbTracking::proto::Pose2DList> call(const std::string& /*methodName*/, boost::shared_ptr<bool> draw_debug) {
 
-    ERROR_MSG("1")
-    // Convert the map to a cv::Mat image
-    cv::Mat map(cv::Size(TS_MAP_SIZE,TS_MAP_SIZE), CV_8UC1); // destination image for sending
-    cv::Mat tmp = cv::Mat(TS_MAP_SIZE, TS_MAP_SIZE, CV_16UC1, static_cast<void*>(&ts_map_.map[0]));
-    tmp.convertTo(map, CV_8UC1, 0.00390625);  // Convert to 8bit depth image
-    for (size_t idx = 0; idx < map.rows * map.cols; ++idx)
-      if (map.at<uint8_t>(idx) > 127) // Delete all unsure guesses
-        map.at<uint8_t>(idx) = 0xFF;
+    // Get the obstacles
+    cv::Mat map = getObstacleMap();
 
-    int erosion_size = 0.1/*m*/ / delta_;
-    cv::Mat element = cv::getStructuringElement( cv::MORPH_ELLIPSE,
-                                           cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                           cv::Point( erosion_size, erosion_size ) );
-    cv::erode( map, map, element );
+    // Expand them
+    const int erosion_size = detectionObstacleErosion / delta_;
+    mapErosion(erosion_size, map);
+
     // Obstacle list
     vector<vector<cv::Point2i> > contours;
 
     // Temporary stuff
     cv::Mat mask, thresholded, debug;
 
-//    if (draw_debug)
-    ERROR_MSG("2")
-    cv::cvtColor(map, debug, CV_GRAY2BGR);
-    ERROR_MSG("3")
+    if (draw_debug) cv::cvtColor(map, debug, CV_GRAY2BGR);
+
     // Get area inside of walls
     cv::threshold(map,thresholded,127,255,cv::THRESH_BINARY);
-    ERROR_MSG("4")
+
     thresholded.copyTo(mask);
-    ERROR_MSG("5")
+
     cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    ERROR_MSG("6")
+
     mask = Mat::zeros(map.size(),CV_8UC1);
-    ERROR_MSG("7")
+
     cv::drawContours(mask, contours, -1, cv::Scalar(255),-1);
-    ERROR_MSG("8")
+
     cv::Mat help = Mat::ones(map.size(),CV_8UC1)*255;
-    ERROR_MSG("9")
+
     thresholded.copyTo(help,mask);
-    ERROR_MSG("3")
+
     // Switch black & white
     cv::Mat objects = Mat::ones(map.size(), CV_8UC1)*255;
     cv::subtract(objects, help, objects);
-    ERROR_MSG("4")
+
     // Find objects
     cv::findContours(objects, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    ERROR_MSG("5")
+
     int numObjects = contours.size();
     cv::Point2f centers[numObjects];
     float objectRadius[numObjects];
     cv::Moments moments;
     boost::shared_ptr<twbTracking::proto::Pose2DList> pose2DList(new twbTracking::proto::Pose2DList);
-    ERROR_MSG("6")
+
     for(int i = 0; i < numObjects; ++i) {
       if (cv::contourArea(contours[i]) < 5) continue;
-    ERROR_MSG("7")
+
       // Calculate objects center of gravity
       moments = cv::moments(contours[i], true);
       centers[i] = Point2f(moments.m10/moments.m00 , moments.m01/moments.m00);
-    ERROR_MSG("8")
+
       // Get objects radius
       cv::Point2f center;
       cv::minEnclosingCircle(contours[i],center,objectRadius[i]);
-    ERROR_MSG("9")
+
       // Add object as pose
       twbTracking::proto::Pose2D *pose2D1 = pose2DList->add_pose();
       pose2D1->set_x(centers[i].x * delta_);
       pose2D1->set_y(centers[i].y * delta_);
       pose2D1->set_orientation(objectRadius[i]);
       pose2D1->set_id(0);
-      ERROR_MSG("10")
-//      if (draw_debug) {
+
+      if (draw_debug) {
         cv::drawContours(debug, contours, i, cv::Scalar(255,191,0),-1);
         cv::circle(debug, centers[i], 3, cv::Scalar(139,0,0),-1);
         cv::circle(debug, center, objectRadius[i], cv::Scalar(0,0,139),1);
-//      }
+      }
     }
 #ifndef __arm__
     if (draw_debug) {
@@ -268,12 +283,13 @@ class objectsCallback: public rsb::patterns::LocalServer::Callback<bool, twbTrac
       cv::waitKey(1);
     }
 #endif
-    std::vector<int> compression_params;
-    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(3);
-    imwrite("detection.png", debug, compression_params);
-    INFO_MSG("Server returns obstacle list")
-
+    if (draw_debug) {
+      std::vector<int> compression_params;
+      compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+      compression_params.push_back(3);
+      imwrite("detection.png", debug, compression_params);
+      INFO_MSG("Server returns obstacle list")
+    }
     return pose2DList;
   }
 };
@@ -282,13 +298,7 @@ class objectsCallback: public rsb::patterns::LocalServer::Callback<bool, twbTrac
 class mapServer: public rsb::patterns::LocalServer::Callback<void, cv::Mat> {
 public:
   boost::shared_ptr<cv::Mat> call(const std::string& /*methodName*/) {
-    // Convert the map to a cv::Mat image
-    boost::shared_ptr<cv::Mat> dst(new cv::Mat(cv::Size(TS_MAP_SIZE,TS_MAP_SIZE), CV_8U)); // destination image for sending
-    cv::Mat image = cv::Mat(TS_MAP_SIZE, TS_MAP_SIZE, CV_16U, static_cast<void*>(&ts_map_.map[0]));
-    image.convertTo(*dst, CV_8U, 0.00390625);  // Convert to 8bit depth image
-    for (size_t idx = 0; idx < dst->rows * dst->cols; ++idx)
-      if (dst->at<uint8_t>(idx) > 127) // Delete all unsure guesses
-        dst->at<uint8_t>(idx) = 0xFF;
+    boost::shared_ptr<cv::Mat> dst(new cv::Mat(getObstacleMap()));
     INFO_MSG("Server returns map")
     return dst;
   }
@@ -500,6 +510,8 @@ int main(int argc, const char **argv){
     ("remoteHost", po::value < std::string > (&remoteHost), "Remote spread daemon host name")
     ("remotePort", po::value < std::string > (&remotePort), "Remote spread daemon port")
     ("senImage", po::value < bool > (&sendMapAsCompressedImage), "Send map as compressed image")
+    ("pathObstacleErosion", po::value < double > (&pathObstacleErosion), "Radius in meter of the erosion of objects for the path planer")
+    ("detectionObstacleErosion", po::value < double > (&detectionObstacleErosion), "Radius in meter of the erosion of objects for the tabletop obstacle detection")
     ("mapAsImageOutScope", po::value < std::string > (&mapAsImageOutScope), "Scope for sending the map as compressed image to a remote spread daemon")
     ("sigma_xy", po::value < double > (&sigma_xy_), "XY uncertainty for marcov localization [m]")
     ("sigma_theta", po::value < double > (&sigma_theta_), "Theta uncertainty for marcov localization [m]")
