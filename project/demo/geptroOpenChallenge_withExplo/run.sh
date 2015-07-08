63db01e4a1b7703f14edc488f823624762eb9891
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-07-08 23:51:46 +0200

    demo: geptroOpenChallenge_withExplo: Added geptro project including exploration.

---
+++ project/demo/geptroOpenChallenge_withExplo/run.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+if [ -z "${1}" ]; then
+  echo "Set a robot ID for the AMiRo"
+  exit 1
+fi
+
+if [ -z "${2}" ]; then
+  echo "Set a tracking ID for the AMiRo"
+  exit 1
+fi
+
+robotID=${1}
+trackingID=${2}
+host=localhost
+port=4823
+prox_obstacle=/rir_prox/obstacle/
+
+# kill maybe already started programs
+./stop.sh
+sleep 1
+
+cpufreq-set -g performance
+
+# start spread
+spread -c amirospread &
+sleep 5
+
+# start all sensing programs
+./rirReader -l > /dev/null &
+./senseFloorProximity &
+
+# start localization programs
+./mapGenerator -r --id $id --host $host --port $port --irin $prox_obstacle &
+./frontierExploration --id $id --host $host --port $port --irin $prox_obstacle &
+
+# start all primary moving programs
+./motorControl > /dev/null &
+
+# start all secondary moving programs
+#./edgeAvoidanceBehavior > /dev/null &
+./localPlannerISY --id $id --host $host --port $port &
+./drivingObjectDetection --useTrackingData --trackingID ${trackingID} --meterPerPixel 0.0025 --trackingInscope /murox/roboterlocation --skipPathPlanner --skipLocalPlanner --skipFinalRotation --skipDetection --skipCorrection --skipLocalization &
+
+# start only listening statemachines
+./answerer --skipDetection &
+./openChallengeGEPTRO_2nd --robotID ${robotID} --testWithAnswerer &
+
+sleep 1
+
+# start commanding statemachine
+./answerer_tobi --robotID ${robotID} &
+
+# just to start exploration
+#sleep 2
+#./rsbsend.sh /exploration start
+
+wait
+cpufreq-set -g ondemand
+
+
+
---
+++ project/demo/geptroOpenChallenge_withExplo/stop.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# kill all commanding statemachines
+killall -9 answerer_tobi
+
+# kill all listening statemachines
+killall -9 openChallengeGEPTRO_2nd
+killall -9 answerer
+
+# kill all secondary moving programs
+killall -9 drivingObjectDetection
+killall -9 localPlannerISY
+#killall -9 edgeAvoidanceBehavior
+
+# kill all primary moving programs
+killall -9 motorControl
+
+# kill localization and exploration
+killall -9 mapGenerator 
+killall -9 frontierExploration 
+
+# kill all sensing programs
+killall -9 rirReader
+killall -9 senseFloorProximity 
+
+# kill spread
+killall -9 spread
+
+sleep 1
+
+# stop all outputs
+./stopAMiRo
+
+
