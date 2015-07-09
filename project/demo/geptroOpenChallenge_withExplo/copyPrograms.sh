180e34c1d227327d5502e3bfc2cebc41be0b1505
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-07-09 11:21:21 +0200

    demo: geptroOpenChallenge_withExplo: Now the saved objectPics are uploaded automatically.

--- project/demo/geptroOpenChallenge_withExplo/copyPrograms.sh
+++ project/demo/geptroOpenChallenge_withExplo/copyPrograms.sh
@@ -7,8 +7,11 @@ ssh root@${IP} "mkdir -p ~/${NAME}"
 for line in `ls -d */ | sed 's#\ ##g' | sed 's#\/##g' | grep -v CMakeFiles`; do
   scp ${line}/${line} root@${IP}:~/${NAME}
 done
+
 scp run.sh root@${IP}:~/${NAME}
 scp stop.sh root@${IP}:~/${NAME}
 # Copy config
 scp amirospread root@${IP}:~/${NAME}
 scp rsb.conf root@${IP}:~/${NAME}
+
+scp -r ${MUROX_PROJECT}/process/objectDetection/objectSavingAMiRo/objectPics root@${IP}:~/${NAME}
