75214e89080f8476a6a0fd1dcf73c14eb87c3210
Florian Patzelt <fpatzelt@techfak.uni-bielefeld.de>
2015-07-09 15:24:22 +0200

    main.cxx adopted from isyproject branch. (reverted from commit 10b10b663910f8d060bea9344fa054e84a726b40)

--- project/process/mapGenerator/main.cxx
+++ project/process/mapGenerator/main.cxx
@@ -58,9 +58,6 @@ const float YM_2_M = 1000000.0;
 // camera parameter
 float meterPerPixel = 1.0 / 400.0;
 
-// radius of the robot
-float robotRadius = 0.05;
-
 // obect used to update the map from sensorvalues
 MapGenerator mapGenerator(cellSize);
 
@@ -136,7 +133,7 @@ class pathCallback: public LocalServer::Callback<twbTracking::proto::Pose2D, twb
 		Point2f target(pose.get()->x(), pose.get()->y());
 
 		// calculate a path
-		robotPose = cv::Point3f(129*cellSize,57*cellSize,0);
+		robotPose = cv::Point3f(129 * cellSize, 57 * cellSize, 0);
 		std::vector<cv::Point2f> path = pathPlanner.getPathToTarget(obstacleMap, robotPose, target);
 
 		// convert that path to a pose2DList
@@ -162,16 +159,16 @@ class objectsCallback: public LocalServer::Callback<void, twbTracking::proto::Po
 		mapGenerator.generateObstacleMap(combinedMap, map);
 
 		// Get area inside of walls
-		cv::threshold(map,thresholded,250,255,cv::THRESH_BINARY);
+		cv::threshold(map, thresholded, 250, 255, cv::THRESH_BINARY);
 		thresholded.copyTo(mask);
 		cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-		mask = Mat::zeros(map.size(),CV_8UC1);
-		cv::drawContours(mask, contours, -1, cv::Scalar(255),-1);
-		cv::Mat help = Mat::ones(map.size(),CV_8UC1)*255;
-		thresholded.copyTo(help,mask);
+		mask = Mat::zeros(map.size(), CV_8UC1);
+		cv::drawContours(mask, contours, -1, cv::Scalar(255), -1);
+		cv::Mat help = Mat::ones(map.size(), CV_8UC1) * 255;
+		thresholded.copyTo(help, mask);
 
 		// Switch black & white
-		cv::Mat objects = Mat::ones(map.size(), CV_8UC1)*255;
+		cv::Mat objects = Mat::ones(map.size(), CV_8UC1) * 255;
 		cv::subtract(objects, help, objects);
 
 		// Find objects
@@ -183,16 +180,17 @@ class objectsCallback: public LocalServer::Callback<void, twbTracking::proto::Po
 		cv::Moments moments;
 		boost::shared_ptr<twbTracking::proto::Pose2DList> pose2DList(new twbTracking::proto::Pose2DList);
 
-		for(int i = 0; i < numObjects; ++i) {
-			if (cv::contourArea(contours[i]) < 5) continue;
+		for (int i = 0; i < numObjects; ++i) {
+			if (cv::contourArea(contours[i]) < 5)
+				continue;
 
 			// Calculate objects center of gravity
 			moments = cv::moments(contours[i], true);
-			centers[i] = Point2f(moments.m10/moments.m00 , moments.m01/moments.m00);
+			centers[i] = Point2f(moments.m10 / moments.m00, moments.m01 / moments.m00);
 
 			// Get objects radius
 			cv::Point2f center;
-			cv::minEnclosingCircle(contours[i],center,objectRadius[i]);
+			cv::minEnclosingCircle(contours[i], center, objectRadius[i]);
 
 			// Add object as pose
 			twbTracking::proto::Pose2D *pose2D1 = pose2DList->add_pose();
@@ -214,20 +212,20 @@ class pushingPathCallback: public LocalServer::Callback<twbTracking::proto::Pose
 		Point3f startPos(inputPointList->pose(0).x(), inputPointList->pose(0).y(), 0);
 		Point2f target(inputPointList->pose(1).x(), inputPointList->pose(1).y());
 
-		cv::Point2f center(inputPointList->pose(2).x()/cellSize, inputPointList->pose(2).y()/cellSize);
-		cv::circle(combinedMap, center, inputPointList->pose(2).orientation()/cellSize, cv::Scalar(255),-1);
+		cv::Point2f center(inputPointList->pose(2).x() / cellSize, inputPointList->pose(2).y() / cellSize);
+		cv::circle(combinedMap, center, inputPointList->pose(2).orientation() / cellSize, cv::Scalar(255), -1);
 
 		// generate the obstacle map
 		Mat obstacleMap;
 		mapGenerator.generateObstacleMap(combinedMap, obstacleMap);
 
 		/*vector<vector<cv::Point2i> > contours;
-		vector<cv::Point2i> contour;
-		for (int i = 2; i < inputPointList->pose_size() - 2; ++i)
-			contour.push_back(Point2i(inputPointList->pose(i).x(), inputPointList->pose(i).y()));
-		contours.push_back(contour);
-		cv::drawContours(obstacleMap, contours, 0, cv::Scalar(255), -1);
-//		cv::drawContours(gridmap, contours, 0, cv::Scalar(255),-1);*/
+		 vector<cv::Point2i> contour;
+		 for (int i = 2; i < inputPointList->pose_size() - 2; ++i)
+		 contour.push_back(Point2i(inputPointList->pose(i).x(), inputPointList->pose(i).y()));
+		 contours.push_back(contour);
+		 cv::drawContours(obstacleMap, contours, 0, cv::Scalar(255), -1);
+		 //		cv::drawContours(gridmap, contours, 0, cv::Scalar(255),-1);*/
 
 		// calculate a path
 		std::vector<cv::Point2f> path = pathPlanner.getPathToTarget(obstacleMap, startPos, target);
@@ -249,12 +247,16 @@ class pushingPathCallback: public LocalServer::Callback<twbTracking::proto::Pose
 };
 
 void insertObject(boost::shared_ptr<twbTracking::proto::Pose2D> objectPtr) {
-	cv::circle(gridmap, cv::Point2f(objectPtr->x()/cellSize,objectPtr->y()/cellSize), (objectPtr->orientation()-robotRadius)/cellSize, cv::Scalar(-255),-1);
+	cv::circle(gridmap, cv::Point2f(objectPtr->x() / cellSize, objectPtr->y() / cellSize),
+			objectPtr->orientation() / cellSize, cv::Scalar(0), -1);
 	combinedMap = gridmap + edgeMap + edgeMap;
 }
 
 void deleteObject(boost::shared_ptr<twbTracking::proto::Pose2D> objectPtr) {
-	cv::circle(gridmap, cv::Point2f(objectPtr->x()/cellSize,objectPtr->y()/cellSize), (objectPtr->orientation()-robotRadius)/cellSize, cv::Scalar(255),-1);
+	cv::Point2f center(objectPtr->x() / cellSize, objectPtr->y() / cellSize);
+	cout << "object: " << center.x << ", " << center.y << endl;
+	cout << "radius: " << objectPtr->orientation() / cellSize << endl;
+	cv::circle(gridmap, center, objectPtr->orientation() / cellSize, cv::Scalar(255), -1);
 	combinedMap = gridmap + edgeMap + edgeMap;
 }
 
@@ -271,7 +273,22 @@ void saveMap(rsb::EventPtr event) {
 	gridmap.convertTo(saveMap, CV_16SC1);
 	saveMap += 128;
 	saveMap.convertTo(saveMap, CV_8UC1);
-	cv::imwrite(*fileName, saveMap,compression_params);
+	cv::imwrite(*fileName, saveMap, compression_params);
+}
+
+void saveEdgeMap(rsb::EventPtr event) {
+	boost::shared_ptr<string> fileName = static_pointer_cast<string>(event->getData());
+	cv::Mat saveMap;
+	vector<int> compression_params;
+	compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+	compression_params.push_back(100);
+	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(0);
+
+	edgeMap.convertTo(saveMap, CV_16SC1);
+	saveMap += 128;
+	saveMap.convertTo(saveMap, CV_8UC1);
+	cv::imwrite(*fileName, saveMap, compression_params);
 }
 
 // get information from tracking
@@ -290,9 +307,11 @@ int main(int argc, char **argv) {
 	std::string mapUpdateScope = "/mapUpdate";
 	std::string serverScope = "/mapGenerator";
 	std::string saveMapInscope = "/saveMap";
+	std::string saveEdgeMapInscope = "/saveEdgeMap";
 	std::string insertObjectInscope = "/mapGenerator/insertObject";
 	std::string deleteObjectInscope = "/mapGenerator/deleteObject";
 	std::string mapFileName = "";
+	std::string edgeMapFileName = "";
 
 	std::string spreadhost = "127.0.0.1";
 	std::string spreadport = "4803";
@@ -311,7 +330,8 @@ int main(int argc, char **argv) {
 			"ID of the tracking marker")("freq,f", po::value<int>(&mapFreq),
 			"Frequency to publish the map (0 = nothing will be published)")("meterPerPixel,mpp",
 			po::value<float>(&meterPerPixel), "Camera parameter: Meter per Pixel")("loadMap,l",
-			po::value<std::string>(&mapFileName), "Load map from file with the given name.")("irin",
+			po::value<std::string>(&mapFileName), "Load map from file with the given name.")("loadEdgeMap,e",
+			po::value<std::string>(&edgeMapFileName), "Load map from file with the given name.")("irin",
 			po::value<std::string>(&proxSensorInscope), "Inscope for IR values")("odoin",
 			po::value<std::string>(&odometryInscope), "Inscope for odometry data")("trackingin",
 			po::value<std::string>(&trackingInscope), "Inscope for tracking pose")("edgein",
@@ -319,7 +339,8 @@ int main(int argc, char **argv) {
 			po::value<std::string>(&enableInscope), "Inscope for enable signals")("updateinout",
 			po::value<std::string>(&mapUpdateScope), "Scope for map updates")("mapout",
 			po::value<std::string>(&occupancyGridOutscope), "Outscope for the occupancy grid map")("saveMapScope",
-			po::value<std::string>(&saveMapInscope), "Scope for filenames to save the map.")("serverScope",
+			po::value<std::string>(&saveMapInscope), "Scope for filenames to save the map.")("saveEdgeMapScope",
+			po::value<std::string>(&saveEdgeMapInscope), "Scope for filenames to save the edge map.")("serverScope",
 			po::value<std::string>(&serverScope), "Scope to setup the server for remote procedure calls.")("host",
 			po::value<std::string>(&spreadhost), "Host for Programatik Spread.")("port",
 			po::value<std::string>(&spreadport), "Port for Programatik Spread.");
@@ -420,13 +441,19 @@ int main(int argc, char **argv) {
 	rsb::ListenerPtr saveMapListener = factory.createListener(saveMapInscope);
 	saveMapListener->addHandler(rsb::HandlerPtr(new rsb::EventFunctionHandler(&saveMap)));
 
+	// prepare RSB listener for commands to save the map
+	rsb::ListenerPtr saveEdgeMapListener = factory.createListener(saveEdgeMapInscope);
+	saveEdgeMapListener->addHandler(rsb::HandlerPtr(new rsb::EventFunctionHandler(&saveEdgeMap)));
+
 	// prepare RSB listener for commands to insert an object in the map
 	rsb::ListenerPtr insertObjectListener = factory.createListener(insertObjectInscope);
-	insertObjectListener->addHandler(rsb::HandlerPtr(new rsb::DataFunctionHandler<twbTracking::proto::Pose2D>(&insertObject)));
+	insertObjectListener->addHandler(
+			rsb::HandlerPtr(new rsb::DataFunctionHandler<twbTracking::proto::Pose2D>(&insertObject)));
 
 	// prepare RSB listener for commands to delete an object from the map
 	rsb::ListenerPtr deleteObjectListener = factory.createListener(deleteObjectInscope);
-	deleteObjectListener->addHandler(rsb::HandlerPtr(new rsb::DataFunctionHandler<twbTracking::proto::Pose2D>(&deleteObject)));
+	deleteObjectListener->addHandler(
+			rsb::HandlerPtr(new rsb::DataFunctionHandler<twbTracking::proto::Pose2D>(&deleteObject)));
 
 	// ---------------- Informer ---------------------
 
@@ -457,6 +484,14 @@ int main(int argc, char **argv) {
 		loadMap.convertTo(loadMap, CV_16SC1);
 		loadMap -= 128;
 		loadMap.convertTo(gridmap, CV_8SC1);
+		combinedMap = gridmap + edgeMap + edgeMap;
+	}
+
+	if (edgeMapFileName != "") {
+		cv::Mat loadMap = cv::imread(edgeMapFileName, -1);
+		loadMap.convertTo(loadMap, CV_16SC1);
+		loadMap -= 128;
+		loadMap.convertTo(edgeMap, CV_8SC1);
 		combinedMap = gridmap + edgeMap + edgeMap;
 	}
 
@@ -499,7 +534,6 @@ int main(int argc, char **argv) {
 				robotPose = readTracking(
 						boost::static_pointer_cast<twbTracking::proto::Pose2DList>(trackingQueue->pop()),
 						trackingMarkerID);
-
 
 				// ignore faulty tracking
 				if (robotPose == cv::Point3f(0, 0, 0)) {
@@ -618,5 +652,4 @@ cv::Point3f readTracking(boost::shared_ptr<twbTracking::proto::Pose2DList> data,
 	}
 	return cv::Point3f(rpose.x() * meterPerPixel, rpose.y() * meterPerPixel, rpose.orientation() * M_PI / 180.0);
 }
-
 
