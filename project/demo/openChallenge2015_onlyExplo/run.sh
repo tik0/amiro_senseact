df156ab7979a08f4fde0215efaa4acb24db9112b
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-07-21 05:24:20 +0200

    demo: openChallenge2015_onlyExplo: Reduced power by lower sensor frequencies and lower number of samples and localization. Additionally gave most programs the robot ID for better map server commuinication.

--- project/demo/openChallenge2015_onlyExplo/run.sh
+++ project/demo/openChallenge2015_onlyExplo/run.sh
@@ -17,35 +17,34 @@ cpufreq-set -g performance
 spread &
 # start spread for remote communication
 spread -c amirospread &
-sleep 5
-spread &
+
 sleep 5
 
 # start all sensing programs
-./senseHokuyo -d /dev/ttyACM0 -o /lidar --publishNthScan 1 > /dev/null &
+./senseHokuyo -d /dev/ttyACM0 -o /lidar --publishNthScan 3 > /dev/null &
 ./senseOdometry -o /odom > /dev/null &
 ./rirReader -l > /dev/null &
 
 # start localization programs
 # ./CoreSLAM --lidarinscope /lidar --localizationOutScope /localization --serverScope /CoreSlamServer --mapServerReq map --remoteHost localhost --remotePort 4823 --senImage 0 --delta 0.01 --hole_width 0.05 &
 # FAULTY # ./CoreSLAM --lidarinscope /lidar --localizationOutScope /localization --serverScope /CoreSlamServer --mapServerReq map --remoteHost localhost --remotePort 4823 --senImage 0 --delta 0.005 --hole_width 0.02 --mapAsImageOutScope /image &
-./CoreSLAM --lidarinscope /lidar --localizationOutScope /localization --serverScope /CoreSlamServer --mapServerReq map --remoteHost localhost --remotePort 4823 --senImage 1 --delta 0.005 --hole_width 0.1 --mapAsImageOutScope /image --samples 500 --throttle_scans 1 --sigma_xy 10.0 --sigma_theta 0.05 &
+./CoreSLAM --robotID ${ID} --lidarinscope /lidar --localizationOutScope /localization --serverScope /CoreSlamServer --mapServerReq map --remoteHost localhost --remotePort 4823 --senImage 0 --delta 0.005 --hole_width 0.1 --mapAsImageOutScope /image --samples 250 --throttle_scans 1 --sigma_xy 10.0 --sigma_theta 0.05 &
 
 # start all primary moving programs
 ./localPlanner -p /localization -i /path -r /pathResponse > /dev/null &
 ./exploDriveEdge -l &
 
 # start all secondary moving programs
-./drivingObjectDetection --bigMap --mapServerIsRemote --skipDetection --mapServerScope /CoreSlamServer --pathRequest path &
-./objectDelivery --useSLAMData --bigMap --positionInscope /localization --pathOut /path --pathRe /pathResponse --mapServer /CoreSlamServer --mapServerIsRemote &
+./drivingObjectDetection --bigMap --mapServerIsRemote --skipDetection --mapServerScope /CoreSlamServer --pathRequest path --robotID ${ID} &
+./objectDelivery --useSLAMData --bigMap --positionInscope /localization --pathOut /path --pathRe /pathResponse --mapServer /CoreSlamServer --mapServerIsRemote --robotID ${ID} &
 
 # start only listening statemachines
 ./answerer --skipExploration --skipDetection --skipDelivery &
-./openChallengeGEPTRO_2nd --robotID ${ID} --mapServerIsRemote --mapServerScope /CoreSlamServer --obstacleServerReq getObjectsList &
+./openChallengeGEPTRO_2nd --robotID ${ID} --bigMap --mapServerIsRemote --mapServerScope /CoreSlamServer --obstacleServerReq getObjectsList &
 
 sleep 1
 
-# start commanding statemachine
+# start commanding statemachine (simulation)
 ./answerer_tobi --robotID ${ID} &
 
 wait
