e82aca021fa9cc60bbce3898a294c37518cdaf3f
AndreasSkiba <Andreas.Skiba@claas.com>
2015-07-30 16:10:08 +0200

    sense: senseIbeoScala_1403: added informer for devicestatus and objectlist

--- project/sense/senseIbeoScala_1403/src/IbeoScalaDataListener.hpp
+++ project/sense/senseIbeoScala_1403/src/IbeoScalaDataListener.hpp
@@ -24,7 +24,9 @@
 // RST
 #include <rsb/converter/ProtocolBufferConverter.h>
 
-class IbeoScalaDataListener : public ibeosdk::DataListener<ibeosdk::Scan2208>
+class IbeoScalaDataListener : public ibeosdk::DataListener<ibeosdk::Scan2208>,
+							  public ibeosdk::DataListener<ibeosdk::DeviceStatus>,
+							  public ibeosdk::DataListener<ibeosdk::ObjectListScala2271>
 
 {
 
@@ -32,14 +34,21 @@ public:
 	IbeoScalaDataListener(std::string ip, std::string scalaScope);
 	virtual ~IbeoScalaDataListener();
 
-	//void onData(const ibeosdk::FrameEndSeparator* const fes);
 	void onData(const ibeosdk::Scan2208* const scan);
+	void onData(const ibeosdk::DeviceStatus* const deviceStatus);
+	void onData(const ibeosdk::ObjectListScala2271* const objs);
 
 private:
 	std::string scalaIp;
 
-	rsb::Informer<rst::claas::IbeoScala_1403>::DataPtr msgIbeoScala;
-	rsb::Informer< rst::claas::IbeoScala_1403 >::Ptr informer;
+	rsb::Informer<rst::claas::IbeoScala_1403_Scan_2208>::DataPtr msgIbeoScala;
+	rsb::Informer< rst::claas::IbeoScala_1403_Scan_2208 >::Ptr informer;
+
+	rsb::Informer<rst::claas::IbeoScala_1403_ObjectData_2271>::DataPtr msgIbeoScalaObjectData_2271;
+	rsb::Informer< rst::claas::IbeoScala_1403_ObjectData_2271 >::Ptr informer_ObjectData_2271;
+
+	rsb::Informer<rst::claas::IbeoScala_1403_DeviceStatus>::DataPtr msgIbeoScalaDeviceStatus;
+	rsb::Informer< rst::claas::IbeoScala_1403_DeviceStatus >::Ptr informer_DeviceStatus;
 };
 
 #endif /* SRC_IBEOSCALADATALISTENER_HPP_ */
