7f64cdf26547793c0e96aacac023d300394d8b95
AndreasSkiba <Andreas.Skiba@claas.com>
2015-08-18 14:33:04 +0200

    tools: MachineModel: updated with new Odemetrie version and reverse driving

--- project/tools/MachineModel/src/Odemetrie_private.h
+++ project/tools/MachineModel/src/Odemetrie_private.h
@@ -1,35 +1,27 @@
-/*
- * File: Odemetrie_private.h
- *
- * Code generated for Simulink model 'Odemetrie'.
- *
- * Model version                  : 1.323
- * Simulink Coder version         : 8.5 (R2013b) 08-Aug-2013
- * C/C++ source code generated on : Wed Jul 15 08:01:34 2015
- *
- * Target selection: ert.tlc
- * Embedded hardware selection: Intel->x86-64
- * Code generation objectives: Unspecified
- * Validation result: Not run
- */
-
+//
+// File: Odemetrie_private.h
+//
+// Code generated for Simulink model 'Odemetrie'.
+//
+// Model version                  : 1.329
+// Simulink Coder version         : 8.7 (R2014b) 08-Sep-2014
+// C/C++ source code generated on : Tue Aug 18 12:53:58 2015
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
 #ifndef RTW_HEADER_Odemetrie_private_h_
 #define RTW_HEADER_Odemetrie_private_h_
 #include "rtwtypes.h"
-#ifndef __RTWTYPES_H__
-#error This file requires rtwtypes.h to be included
-#else
-#ifdef TMWTYPES_PREVIOUSLY_INCLUDED
-#error This file requires rtwtypes.h to be included before tmwtypes.h
-#endif                                 /* TMWTYPES_PREVIOUSLY_INCLUDED */
-#endif                                 /* __RTWTYPES_H__ */
 
 extern real_T rt_roundd_snf(real_T u);
 
-#endif                                 /* RTW_HEADER_Odemetrie_private_h_ */
+#endif                                 // RTW_HEADER_Odemetrie_private_h_
 
-/*
- * File trailer for generated code.
- *
- * [EOF]
- */
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
--- project/tools/MachineModel/src/Odemetrie_types.h
+++ project/tools/MachineModel/src/Odemetrie_types.h
@@ -1,32 +1,31 @@
-/*
- * File: Odemetrie_types.h
- *
- * Code generated for Simulink model 'Odemetrie'.
- *
- * Model version                  : 1.323
- * Simulink Coder version         : 8.5 (R2013b) 08-Aug-2013
- * C/C++ source code generated on : Wed Jul 15 08:01:34 2015
- *
- * Target selection: ert.tlc
- * Embedded hardware selection: Intel->x86-64
- * Code generation objectives: Unspecified
- * Validation result: Not run
- */
-
+//
+// File: Odemetrie_types.h
+//
+// Code generated for Simulink model 'Odemetrie'.
+//
+// Model version                  : 1.329
+// Simulink Coder version         : 8.7 (R2014b) 08-Sep-2014
+// C/C++ source code generated on : Tue Aug 18 12:53:58 2015
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
 #ifndef RTW_HEADER_Odemetrie_types_h_
 #define RTW_HEADER_Odemetrie_types_h_
 #include "rtwtypes.h"
 
-/* Parameters (auto storage) */
+// Parameters (auto storage)
 typedef struct P_Odemetrie_T_ P_Odemetrie_T;
 
-/* Forward declaration for rtModel */
+// Forward declaration for rtModel
 typedef struct tag_RTM_Odemetrie_T RT_MODEL_Odemetrie_T;
 
-#endif                                 /* RTW_HEADER_Odemetrie_types_h_ */
+#endif                                 // RTW_HEADER_Odemetrie_types_h_
 
-/*
- * File trailer for generated code.
- *
- * [EOF]
- */
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
