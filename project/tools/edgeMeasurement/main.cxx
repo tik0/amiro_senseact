0c4e7325086adcb8ee2703993934a2401a9a1ee9
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-12-08 21:14:06 +0100

    tools: edgeMeasurement: Added documentation.

--- project/tools/edgeMeasurement/main.cxx
+++ project/tools/edgeMeasurement/main.cxx
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : main.cxx
-// Author      : mbarther
-// Description : -
+// Author      : mbarther <mbarther@techfak.uni-bielefeld.de>
+// Description : Tool for printing the measured edge distance.
 //============================================================================
 
 //#define TRACKING
@@ -11,6 +11,7 @@
 #define WARNING_MSG_
 // #define ERROR_MSG_
 #include <MSG.h>
+
 #include <functional>
 #include <algorithm>
 #include <math.h>
@@ -18,49 +19,13 @@
 #include <iostream>
 #include <boost/thread.hpp>
 #include <boost/program_options.hpp>
-//#include <boost/shared_ptr.hpp>
 #include <boost/date_time.hpp>
 
-/*
-#include <rsb/filter/OriginFilter.h>
-#include <rsb/Informer.h>
-#include <rsb/Factory.h>
-#include <rsb/Event.h>
-#include <rsb/Handler.h>
-#include <rsb/converter/Repository.h>
-#include <rsb/converter/ProtocolBufferConverter.h>
-#include <rsc/threading/SynchronizedQueue.h>
-#include <rsb/util/QueuePushHandler.h>
-
-
-using namespace rsb;
-
-#include <rst/geometry/Pose.pb.h>
-#include <rst/geometry/Translation.pb.h>
-#include <rst/geometry/Rotation.pb.h>
-using namespace rst::geometry;
-
-#include <converter/vecIntConverter/main.hpp>
-//#include <converter/matConverter/matConverter.hpp>
-using namespace muroxConverter;
-*/
 using namespace std;
-/*
-#include <Types.h>
-
-#include <types/twbTracking.pb.h>
-*/
 
 #include <stdint.h>  // int32
 
 #include <ControllerAreaNetwork.h>
-
-//using namespace rsb;
-//using namespace rsb::patterns;
-
-// edge model 5cm
-//#define EDGEMODEL_M 5.789049282390683
-//#define EDGEMODEL_B 0.556285015097247
 
 // edge model 6cm
 #define EDGEMODEL_M 6.359946153158588
@@ -176,7 +141,7 @@ int main(int argc, char **argv) {
 
   po::options_description options("Allowed options");
   options.add_options()("help,h", "Display a help message.")
-      ("loadOffsets,l", "Loads offset from the file 'irConfig.conf'.");
+      ("loadOffsets,l", "Loads offsets from the file 'irConfig.conf' and 'irEmpty.conf'.");
 
   // allow to give the value as a positional argument
   po::positional_options_description p;
