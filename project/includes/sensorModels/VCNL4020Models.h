fac3c64cbe11c40a3b07fc5eb874fb545265030c
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2015-12-15 11:08:25 +0100

    includes: sensorModels: VCNL4020Models: Added obstacle error model and defined types of constants.

--- project/includes/sensorModels/VCNL4020Models.h
+++ project/includes/sensorModels/VCNL4020Models.h
@@ -2,17 +2,24 @@
 #define VCNL4020Models
 
 namespace VCNL4020Models {
-	#define VCNL4020MODELS_OBSTACLEMODEL_ALPHA 0.942693757414292
-	#define VCNL4020MODELS_OBSTACLEMODEL_BETA -16.252241893638708
-	#define VCNL4020MODELS_OBSTACLEMODEL_DELTA 0
-	#define VCNL4020MODELS_OBSTACLEMODEL_XI 1.236518540376969
-	#define VCNL4020MODELS_OBSTACLEMODEL_MEAS_VARIANCE 20.886268537074187 //7.283035954107597
+	/** \brief Alpha constant of the obstacle model */
+	static const float VCNL4020MODELS_OBSTACLEMODEL_ALPHA = 0.942693757414292;
+	/** \brief Beta constant of the obstacle model */
+	static const float VCNL4020MODELS_OBSTACLEMODEL_BETA = -16.252241893638708;
+	/** \brief Delta constant of the obstacle model */
+	static const float VCNL4020MODELS_OBSTACLEMODEL_DELTA = 0.0;
+	/** \brief Xi constant of the obstacle model */
+	static const float VCNL4020MODELS_OBSTACLEMODEL_XI = 1.236518540376969;
+	/** \brief Measurement variance of the obstacle model */
+	static const float VCNL4020MODELS_OBSTACLEMODEL_MEAS_VARIANCE = 20.886268537074187;
 
-	#define VCNL4020MODELS_EDGEMODEL_M 6.359946153158588
-	#define VCNL4020MODELS_EDGEMODEL_B 0.401918238192352
+	/** \brief Gradient m of the edge model */
+	static const float VCNL4020MODELS_EDGEMODEL_M = 6.359946153158588;
+	/** \brief Axis intercept b of the edge model */
+	static const float VCNL4020MODELS_EDGEMODEL_B = 0.401918238192352;
 
-	// calculates the distance [m] to an obstacle given the obstacles angle [rad] relative to the sensor and the sensor value [ticks]
-	float obstacleModel(float angle, float sensorValue) {
+	/** \brief Calculates the distance [m] to an obstacle, given the obstacle's angle [rad] relative to the sensor's normal and the sensor value [ticks] */
+	static float obstacleModel(float angle, float sensorValue) {
 		float cosxi = cos(VCNL4020MODELS_OBSTACLEMODEL_XI * angle);
 		if (cosxi < 0) {
 			cosxi *= -1;
@@ -24,8 +31,20 @@ namespace VCNL4020Models {
 		return sqrt(VCNL4020MODELS_OBSTACLEMODEL_ALPHA * cosxi / divi + VCNL4020MODELS_OBSTACLEMODEL_DELTA * cosxi);
 	}
 
-	// calculates the distance [m] to the next edge given the normalized edge value factored by 10000 (as integer)
-	float edgeModel(int senValue) {
+	/** \brief Calculates the standard deviation [m] of the calculated distance of the obstacle model, given the calculated distance [m] to the obstacle and the obstacle's angle [rad] relative to the sensor's normal */
+	static float obstacleErrorModel(float dist, float angle) {
+		float sig = sqrt(VCNL4020MODELS_OBSTACLEMODEL_MEAS_VARIANCE);
+		float cosxi = cos(VCNL4020MODELS_OBSTACLEMODEL_XI * angle);
+		if (cosxi < 0) {
+			cosxi *= -1;
+		}
+		float diffdistdelta = dist * dist / cosxi - VCNL4020MODELS_OBSTACLEMODEL_DELTA;
+		float error = (diffdistdelta * diffdistdelta) / (2 * dist * VCNL4020MODELS_OBSTACLEMODEL_ALPHA * sqrt(1 / cosxi)) * sig;
+		return error;
+	}
+
+	/** \brief Calculates the distance [m] to the next edge, given the normalized edge value factored by 10000 (as integer) */
+	static float edgeModel(int senValue) {
 		return (VCNL4020MODELS_EDGEMODEL_M * ((float)senValue)/10000.0 + VCNL4020MODELS_EDGEMODEL_B) / 100;
 	}
 }
