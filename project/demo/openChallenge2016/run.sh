64ec622b0cc21b9ebb2770fabb6403e7d3d2476c
jexner <jexner@techfak.uni-bielefeld.de>
2016-04-04 20:12:18 +0200

    openChallenge2016: add configuration as it was used in the finals

--- project/demo/openChallenge2016/run.sh
+++ project/demo/openChallenge2016/run.sh
@@ -22,7 +22,7 @@ sleep 5
 
 # sensing camera from 'project/sense/senseCamJpg/'
 # v4l2-ctl -d/dev/v4l-subdev8 --set-ctrl=vertical_flip=1
-# ./senseCamJpg -d /dev/video6 -o /cam &
+# ./senseCamJpg -d 6 -o /cam &
 
 # sensing lidar from 'project/sense/senseHokuyo/'
 ./senseHokuyo -d /dev/ttyACM0 -o /lidar &
@@ -40,12 +40,13 @@ sleep 5
 ./sendOdometryProtoPose --resetodom true -o /odom &
 
 ./CoreSLAM --odominscope /odom --lidarinscope /lidar --senImage 1 --delta 0.05 --sigma_xy 0.1 --sigma_xy_new_position 0.1 --sigma_theta 0.01 --sigma_theta_new_position 0.01  --remotePort 4823 --doMapUpdate false --loadMapWithValidPositionsFromPNG ./data/arenaEindhovenValid-final-stacking.png --loadMapFromImage ./data/arenaEindhoven-final-stacking.png --hominginscope /homing --flipHorizontal true \
-  --initialX 22093.8 --initialY 22531.2 --initialTheta 38.1572 &
+  --initialX 21656.2 --initialY 22050.0 --initialTheta 36.4692 \
+  --mapAsImageOutScope /CoreSLAMLocalization/image/${ID} &
 
-./actTargetPosition --inscope /targetPositions
+./actTargetPosition --inscope /targetPositions &
 
 # webserver running on port 80
-# ./rsb_ws_bridge_amiro &
+#./rsb_ws_bridge_amiro &
 
 wait
 cpufreq-set -g ondemand
--- project/demo/openChallenge2016/show-map.sh
+++ project/demo/openChallenge2016/show-map.sh
@@ -5,5 +5,5 @@ export RSB_TRANSPORT_SPREAD_HOST=localhost
 export RSB_TRANSPORT_SPREAD_ENABLED=1
 
 rsb-sendcpp0.11 /sendMap <(echo enable)
-../../tools/hmi/setPosition/setPosition --i /CoreSLAMLocalization/image --flipHorizontal
+../../tools/hmi/setPosition/setPosition --i /CoreSLAMLocalization/image/$1 --flipHorizontal
 rsb-sendcpp0.11 /sendMap <(echo disable)
