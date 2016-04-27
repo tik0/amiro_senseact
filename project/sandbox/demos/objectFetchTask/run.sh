c0d98346ac8592850dee69a339a829d3ae0ec5ce
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2016-04-27 12:35:09 +0200

    sandbox: demos: objectFetchTask: Removed unused parameter in run.sh.

--- project/sandbox/demos/objectFetchTask/run.sh
+++ project/sandbox/demos/objectFetchTask/run.sh
@@ -7,7 +7,7 @@ cpufreq-set -g performance
 
 ./senseFloorProximity -o /prox/floor > /dev/null &
 sleep 1
-./objectFetchTask --useFakeObject -t 100 -p /prox/floor
+./objectFetchTask --useFakeObject -p /prox/floor
 
 wait
 cpufreq-set -g ondemand
