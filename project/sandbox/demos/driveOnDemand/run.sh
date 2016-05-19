5d2f973487ae13a35b8478bd0ae8122b0dd82aba
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2016-05-19 11:05:37 +0200

    sandbox: demos: driveOnDemand: Added port and host for RSB communication and added some outputs.

--- project/sandbox/demos/driveOnDemand/run.sh
+++ project/sandbox/demos/driveOnDemand/run.sh
@@ -9,7 +9,7 @@ spread -c amirospread &
 
 sleep 5
 
-./driveOnDemand &
+./driveOnDemand --port 4823 --host localhost &
 
 #wait
 #cpufreq-set -g ondemand
--- project/sandbox/driveOnDemand/main.cxx
+++ project/sandbox/driveOnDemand/main.cxx
@@ -205,6 +205,7 @@ int main(int argc, char **argv) {
 		myCAN.setLightColor(led, curColor);
 	}
 
+	INFO_MSG("Starting driving loop");
 	while (!exitProg) {
 		// wait for new guide command
 		while (guideQueue->empty() && !exitProg) {
