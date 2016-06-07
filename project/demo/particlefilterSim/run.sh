77968ec73514790691e8eadea44e54df7d0844ef
jexner <jexner@techfak.uni-bielefeld.de>
2016-06-07 22:29:13 +0200

    demo: particlefilterSim: add T-shaped map

--- project/demo/particlefilterSim/run.sh
+++ project/demo/particlefilterSim/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+trap './stop.sh; exit' EXIT
+
 ./stop.sh
 
 # RSB scopes
 lidarscope='/AMiRo_Hokuyo/lidar'
-odomscope='/AMiRo_Hokuyo/gps'
+odomscope='/AMiRo_Hokuyo/odom'
 kinscope='/AMiRo_Hokuyo/diffKin'
 
 # start spread
@@ -12,15 +14,16 @@ spread &
 sleep 5
 
 # start gazebo
-gazebo ./data/enclosed_world-no-cylinder.world &
+gazebo ./data/T.world &
 
 # start particle filter
 #valgrind --tool=callgrind \
 ./particlefilter/particlefilter \
   --lidarInScope "$lidarscope" --odomInScope "$odomscope" \
-  --pathToMap ./data/enclosed_world-cropped.png --meterPerPixel 0.01 \
-  --sampleCount 100 \
-  --beamskip 2 \
+  --pathToMap ./data/T-scale-0.5.png --meterPerPixel 0.02 \
+  --sampleCount 500 \
+  --newSampleProb 0.1 \
+  --beamskip 1 \
   --debugImageOutScope /image &
 
 # start keyboard control
