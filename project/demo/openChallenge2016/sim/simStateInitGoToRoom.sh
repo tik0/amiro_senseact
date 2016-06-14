f967b8d954bc2aff0f88519cd954b22da028240a
jexner <jexner@techfak.uni-bielefeld.de>
2016-06-14 19:30:51 +0200

    demo: openChallenge2016: add simulation scripts for spreading to rooms

---
+++ project/demo/openChallenge2016/sim/simStateInitGoToRoom.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z "${1}" ]; then
+  ID=""
+else
+  ID=${1}
+fi
+
+if [ "${ID}" -eq "${ID}" ] > /dev/null ; then
+  rsb-sendcpp0.11 /tobiamiro${ID}/state states/initGoToRoom.txt
+  echo "Send 'initGoToRoom' to AMiRo ${ID}"
+else
+  for num in `seq 0 9` ; do
+    rsb-sendcpp0.11 /tobiamiro${num}/state states/initGoToRoom.txt
+    echo "Send 'initGoToRoom' to AMiRo ${num}"
+  done
+fi
