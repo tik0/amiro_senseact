68ebaf3157c22f146ed7fa501cfd36576ba66a12
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2016-06-16 20:29:57 +0200

    sense: Add senseSickTim sensor driver

---
+++ project/sense/senseSickTim/include/sick_tim/abstract_parser.h
@@ -0,0 +1,30 @@
+#ifndef ABSTRACT_PARSER_H_
+#define ABSTRACT_PARSER_H_
+
+#include <sick_tim/SickTimConfig.h>
+// RST Proto types
+#include <types/LocatedLaserScan.pb.h>
+#include <rst/geometry/Pose.pb.h>
+
+namespace sick_tim
+{
+
+enum ExitCode
+{
+    ExitSuccess = 0
+    , ExitError = 1    // non-fatal error, retry
+    , ExitFatal = 2    // fatal error, exit
+};
+
+class AbstractParser
+{
+public:
+  AbstractParser();
+  virtual ~AbstractParser();
+
+  virtual int parse_datagram(char* datagram, size_t datagram_length, SickTimConfig &config,
+                             rst::vision::LocatedLaserScan &msg) = 0;
+};
+
+} /* namespace sick_tim */
+#endif /* ABSTRACT_PARSER_H_ */
