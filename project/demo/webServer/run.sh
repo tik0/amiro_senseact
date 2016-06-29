35cdec31f405d21551cba5897abf906b653d2ad0
Marvin Barther <mbarther@techfak.uni-bielefeld.de>
2016-06-29 22:48:01 +0200

    demo:webServer: Updates amiro webserver to RSB 0.13 and added RGBD camera and Sick Laser Scanner.

--- project/demo/webServer/run.sh
+++ project/demo/webServer/run.sh
@@ -10,10 +10,16 @@ sleep 5
 
 # sensing camera from 'project/sense/senseCamJpg/'
 v4l2-ctl -d/dev/v4l-subdev8 --set-ctrl=vertical_flip=1
-./senseCamJpg -d /dev/video6 -o /cam &
+./senseCamJpg -d 6 -o /cam &
+
+# sensing rgbd camera
+./senseRGBD --rgbMode 0 --depthMode 0 -si -r /rgbd &
 
 # sensing lidar from 'project/sense/senseHokuyo/'
 ./senseHokuyo -d /dev/ttyACM0 -o /lidar &
+
+# sensing sick
+./senseSick -o /lidar > /dev/null &
 
 # webserver running on port 80
 ./rsb_ws_bridge_amiro &
