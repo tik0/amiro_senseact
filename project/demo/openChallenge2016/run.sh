28a56553a0f19b00116b652b4024fe6c045a1054
jexner <jexner@techfak.uni-bielefeld.de>
2016-09-20 16:59:41 +0200

    demo: openChallenge2016: swap initial poses and clean up of run.sh

--- project/demo/openChallenge2016/run.sh
+++ project/demo/openChallenge2016/run.sh
@@ -58,7 +58,7 @@ start_spread
 wait_for_port 4823
 start_spread -c amirospread
 
-# sensing lidar from 'project/sense/senseHokuyo/'
+# sensing lidar
 if [ -e /dev/ttyACM0 ]; then
 	echo "Assuming Hokuyo laser scanner"
 	./senseHokuyo -d /dev/ttyACM0 -o /lidar > /dev/null &
@@ -67,64 +67,31 @@ else
 	./senseSickTim -o /lidar > /dev/null &
 fi
 
-# sense ringproximty
-#./senseRingProximity --outscopeObstacle /rir_prox/obstacle --noEdgeValues --period 100 &
-
-# waypoint program from 'project/sandbox/waypoint/'
-#./waypoint --lidarinscope /lidar --range 1.5 --removeLessThan 9 --splitConnected 0.06 &
-# 818 - 409
+# waypoint program
 sleep 1 # sleep so we don't get old laser values
 ./waypoint --lidarinscope /lidar --range 1.5 --scanStartIndex 390 --scanEndIndex 420 -s > /dev/null &
 
 # start state machine
-poseXThresholds="17.5
-19"
+poseXThresholds="19.0
+17.0"
 
 poseXThreshold="$(echo "$poseXThresholds" | head -n $((${ID} + 1 % 4)) | tail -n 1)"
 ./final2016 --id ${ID} --poseScope /amiro${ID}/pose --poseXThreshold $poseXThreshold > /dev/null &
 
+# sense odometry data
 ./sendOdometryProtoPose --resetodom true -o /odom > /dev/null &
 
 # start follower
-# default values:
-# followMinDist: 300 mm
-# followMinBackDist: 200 mm
-# followDistSlowingDown: 200 mm
-# forwardSpeed: 500 mm/s
-# forwardMinSpeed: 150 mm/s
-# turningSpeed: 100 mrad/s
-# turnCorrectSpeed: 60 mrad/s
-# maxRange: 2000 mm
-# rotationTolarence: PI/36 ~= 5°
-./follow_LaserScanner -l /lidar --forwardSpeed 800 --forwardMinSpeed 400 --followMinDist 400 --followDistSlowingDown 400 --followMinBackDist 200 --maxRange 1500 --turningSpeed 50 --maxAngle 180 &
+./follow_LaserScanner -l /lidar --forwardSpeed 800 --forwardMinSpeed 400 --followMinDist 400 --followDistSlowingDown 400 --followMinBackDist 200 --maxRange 1500 --turningSpeed 50 &
 
-# 0 1
-initialPoses="16467.2 18082.7 45
-17677.1 19037.1 -45"
+# initial poses for CoreSLAM
+initialPoses="17677.1 19037.1 -45
+16467.2 18082.7 45"
 
 initialPose="$(echo "$initialPoses" | head -n $((${ID} + 1 % 4)) | tail -n 1)"
 initialX="$(echo "$initialPose" | cut -d\  -f 1)"
 initialY="$(echo "$initialPose" | cut -d\  -f 2)"
 initialTheta="$(echo "$initialPose" | cut -d\  -f 3)"
-
-# positions:
-# 0: corridor
-# 1: dining_room
-# 2: living_room
-# 3: kitchen
-
-corridor="27830.3 14000.7 -90"
-kitchen="25500.8 15824.3 142.125"
-dining_room="28366.7 14903.5 0"
-living_room="31807.2 18491.4 -90"
-
-targetPoses="$corridor
-$dining_room
-$living_room
-$kitchen"
-
-# take row ID + 1
-targetPose="$(echo "$targetPoses" | head -n $((${ID} + 1 % 4)) | tail -n 1)"
 
 ./CoreSLAM --odominscope /odom --lidarinscope /lidar --hominginscope /homing --mapAsImageOutScope /CoreSLAMLocalization/image --setPositionScope /setPosition/${ID} --targetPoseInScope /setTargetPose/${ID} --positionOutScope /amiro${ID}/pose --pathOutScope /amiro${ID}/path \
   --remotePort 4823 \
@@ -133,9 +100,8 @@ targetPose="$(echo "$targetPoses" | head -n $((${ID} + 1 % 4)) | tail -n 1)"
   --loadMapFromImage ./data/homecomingCitec.pgm --flipHorizontal true \
   --erosionRadius 0.35 \
   --initialX $initialX --initialY $initialY --initialTheta $initialTheta \
-  --targetPose "$targetPose" \
   --precomputeOccupancyMap true \
-  --targetSpeed0 0 --targetSpeed1 1.5 > /dev/null &
+  --targetSpeed0 0 --targetSpeed1 1.5 &
 
 ./actEmergencyStop --lidarinscope /lidar --cntMax 15 --distance 0.25 --delay 10 --switchinscope /following > /dev/null &
 
