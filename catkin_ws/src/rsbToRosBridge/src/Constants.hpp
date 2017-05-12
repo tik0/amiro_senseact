31cfedc1f0a213368026c03042e4685bbbc88255
Jonas Dominik Homburg <jhomburg@techfak.uni-bielefeld.de>
2017-05-12 11:43:08 +0200

    small changes

--- catkin_ws/src/rsbToRosBridge/src/Constants.hpp
+++ catkin_ws/src/rsbToRosBridge/src/Constants.hpp
@@ -4,5 +4,9 @@
 namespace constants {
 	std::string rsbImageScope = "/image";
 	std::string rosImageTopic = "/image";
+	std::string rsbFloorProxScope = "/prox/floor";
+	std::string rosFloorProxTopic = "/prox/floor";
+	std::string rsbRingProxScope = "/rir_prox";
+	std::string rosRingProxTopic = "/rir_prox";
 }
 #endif
