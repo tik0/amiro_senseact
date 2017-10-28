9cb8585ba95ae6b8f8dbd5f152fbef08823d772b
Jonas Dominik Homburg <jhomburg@techfak.uni-bielefeld.de>
2017-10-28 19:59:06 +0200

    fixed typo

--- catkin_ws/map_evaluation/ROCTool/ROCSaveToFile.py
+++ catkin_ws/map_evaluation/ROCTool/ROCSaveToFile.py
@@ -2,7 +2,7 @@ from collections import deque
 
 def saveToCSV(filename,ROCs):
     csvFile = open(filename,"w")
-    csvFile.write("fpr,fnr,mr,filename\n")
+    csvFile.write("fpr,tpr,mr,filename\n")
     for r in ROCs:
         csvFile.write("%f,%f,%f,%s,%s\n"%(r[1][0],r[1][1],r[2],r[0],r[3]))
     csvFile.close()
